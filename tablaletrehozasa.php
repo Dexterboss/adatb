76f1db4a0342ac06532847cc6e8d3aaf5f2232e3
Desterboss <rolandmagyar78@gmail.com>
2025-03-25 08:40:53 +0100

    Táblalétrehozás teszt előtt

--- tablaletrehozasa.php
+++ tablaletrehozasa.php
@@ -14,6 +14,33 @@ else
 {
     echo"Csatlakozás megtörtént";
 }
-
-
+$sql = "DROP TABLE IF EXISTS emberek";
+if ($conn->query($sql) === TRUE)
+{
+    echo "Sikeres táblatörtlés";
+}
+else
+{
+    echo "Nem sikerült a tábla törlés";
+}
+$sql = "";
+$sql="use enabn;
+DROP TABLE IF EXISTS emberek;
+CREATE TABLE IF NOT EXISTS emberek(
+    id INT unsigned AUTO_INCREMENT PRIMARY KEY, 
+	vezeteknev VARCHAR(30) NOT NULL, 
+    keresztnev VARCHAR(30) NOT NULL, 
+    beosztas VARCHAR(50) NOT NULL, 
+    RegisztDatum TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+";
+if ($conn->query($sql) === TRUE)
+{
+    echo "Sikeres tábla létrehozás";
+}
+else
+{
+    echo "Nem sikerült a tábla létrehozása";
+}
+$conn->close();    
 
